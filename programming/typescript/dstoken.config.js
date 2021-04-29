6be877f5940d5d74901fb69bf0571d3dfba05816
lionel_lonkaptsamba <lktslionel@gmail.com>
2021-04-30 00:23:50 +0200

    ts: Prototyping dstoken config file

--- programming/typescript/dstoken.config.js
+++ programming/typescript/dstoken.config.js
@@ -1,41 +1,41 @@
 
 
-module.exports = {
+// module.exports = {
   
-  name: "",
-  sources: [
-    {
-      plateform: "figma|sketch",
-      type: "api",
-      options: {}
-    },
-    {
-      plateform: "json",
-      type: "generic",
-      options: {}
-    }
-  ],
-  targets: [
-    {
-      plateform: "sass",
-      type: "lang",
-      options: {}
-    },
-    {
-      plateform: "sass",
-      type: "lang",
-      options: {}
-    },
-    {
-      plateform: "android",
-      type: "os",
-      options: {}
-    }
-  ],
-  plugins: [
+//   name: "",
+//   sources: [
+//     {
+//       plateform: "figma|sketch",
+//       type: "api",
+//       options: {}
+//     },
+//     {
+//       plateform: "json",
+//       type: "generic",
+//       options: {}
+//     }
+//   ],
+//   targets: [
+//     {
+//       plateform: "sass",
+//       type: "lang",
+//       options: {}
+//     },
+//     {
+//       plateform: "sass",
+//       type: "lang",
+//       options: {}
+//     },
+//     {
+//       plateform: "android",
+//       type: "os",
+//       options: {}
+//     }
+//   ],
+//   plugins: [
     
-  ]
-}
+//   ]
+// }
 
 // export default {
 //   sources: {
@@ -59,3 +59,92 @@ module.exports = {
     
 //   } 
 // }
+
+
+
+// export default {
+//   sources: {
+//     figma: {
+//       api_token: process.env.DSTOKEN_FIGMA_API_TOKEN
+//       options: {}
+//     },
+//     json: {
+//       options: {}
+//     }
+//   },
+//   builders: {
+//     style_dictionary: {
+
+//     }
+//   },
+//   targets: {
+//     sass: {
+      
+//     },
+//     android: {
+
+//     }
+//   },
+//   plugins: {
+    
+//   }
+// }
+
+
+module.exports = function (config) {
+
+  config.builders = {
+    styleDictionary: {
+
+    },
+    customBuilder: {
+
+    }
+  }
+
+  config.sources = {
+    figma: {
+      platform: "figma",
+      type: "api",
+      options: {}
+    }
+  }
+
+  config.tasks = [
+    {
+      // builder: {
+      //   style_dictionary: {
+
+      //   }
+      // },
+      sources: [
+        config.sources.figma,
+        {
+          platform: "json",
+          type: "generic",
+          options: {}
+        }
+      ],
+      targets: [
+        {
+          platform: "android",
+          type: "os",
+          builder: config.builders.custom_builder,
+          options: {
+            transforms: [],
+            buildPath: [],
+          }
+        }, 
+        {
+          platform: "scss",
+          type: "generic",
+          // builder: config.builders.style_dictionary, // By default
+          options: {}
+        } 
+      ]
+    }
+  ]
+
+  return config;
+
+}
