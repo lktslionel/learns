b34261c82c08c021a68793a896ed314246b49d9c
lionel_lonkaptsamba <lktslionel@gmail.com>
2021-05-11 12:05:53 +0200

    ts: learn OOP

--- programming/typescript/code/oop.ts
+++ programming/typescript/code/oop.ts
@@ -3,24 +3,85 @@
 
 class Person {
 
-  constructor(private name: string) {
+  // constructor(private readonly name: string) {
+  //   this.name = name
+  // }
+
+  constructor(protected name: string) {
     this.name = name
   }
 
 }
 
 class Employee extends Person {
-  constructor(name: string, public readonly companyName: string) {
-    super(name)
+
+  private static instance: Employee
+
+  get fullname() {
+    return this.name
+  }
+
+  constructor(fullname: string, public readonly companyName: string) {
+    super(fullname)
     this.companyName = companyName
+  }
+
+  static generateId() :string {
+    return Math.random().toString(36).substring(7);
+  }
+
+  static getInstance() {
+    if (! Employee.instance) {
+      Employee.instance = new Employee("none", "ACME")
+      return Employee.instance
+    }
+    
+    return Employee.instance
   }
 }
 
 
+//
+// GETTERS & SETTERS
+//
 
 
-console.log(new Person("Lionel"))
+// class Employee extends Person {
 
-const e = new Employee("Lonkap", "e.Voyageurs SNCF")
+//   get fullname() {
+//     return this.name
+//   }
 
-console.log(e.companyName)
+//   set fullname(value: string) {
+//     this.fullname = name
+//   }
+
+//   constructor(fullname: string, public readonly companyName: string) {
+//     super(fullname)
+//     this.companyName = companyName
+//   }
+// }
+
+
+
+// console.log(new Person("Lionel"))
+
+// const e = new Employee("Lonkap", "e.Voyageurs SNCF")
+
+// console.log(e.companyName)
+// console.log(e.fullname)
+
+// console.log(`Id: ${Employee.generateId()}`)
+
+//
+// Singleton 
+//
+
+import { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
+
+Deno.test("Employee must support singleton pattern", () => {
+  const e1 = Employee.getInstance() 
+  const e2 = Employee.getInstance() 
+  
+  assertEquals(e1, e2);
+});
